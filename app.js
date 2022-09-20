aaae970f559a586c2fba7a3e417a83c994066c5e
letuantai15f <letuantai15f@gmail.com>
2022-09-20 16:55:02 +0700

    tai_dangnhap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+app.use(express.static('resources'));
 app.engine('hbs', exphbs.engine());
 app.set('view engine', 'hbs');
 
